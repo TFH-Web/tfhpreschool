5459b01c1407e057d1d484e85f5e377562640e6f
Tim Peterson <timothypeterson@live.com>
2019-01-23 09:09:22 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="The Father's House Kids Preschool, Vacaville, CA">
+		<meta name="author" content="TFH Kids Preschool">
+		<link rel="icon" type="image/png" href="favicon.ico">
+		<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+		
+		<title>TFH Kids Preschool</title>
+		
+		<link href="-/css/bootstrap.css" rel="stylesheet">
+		<link href="-/css/tfkpreschool.css" rel="stylesheet">
+		
+		<script src="https://use.typekit.net/sxk4dgi.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+	</head>
+
+	<body>
+		<header class="header big-photo">
+			<h1>The Father's Kids</h1>
+			<h2>Preschool</h2>
+
+			<div class="logo-container">
+				<img src="-/img/tfhpreschool-logo-white.png" alt="TFH Kids Preschool" class="logo" />
+			</div>
+
+			<!--<a class="btn btn-success btn-pay" href="https://secure.headmasteronline.com/hmolweb/login.aspx?sitenumber=160103">Pay Tuition</a>-->
+			<a class="btn btn-primary btn-register" href="">Registration Opens Feb 1</a>
+		</header>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-lg-10 col-lg-offset-1" id="mission">
+					<div class="jumbotron">
+						<p>We are a Christian preschool cultivating a lifelong love of learning by creating a safe and loving enviroment to grow. We strive to ignite a confidence and love of learning that each child will carry with them for the rest of their lives.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="big-photo" style="background-image: url(-/img/1200/photo-3-bg.jpg); margin-bottom: 20px;">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="jumbotron">
+							<div class="col-xs-12 col-sm-6">
+								<p>We strive for excellence in your child's early education experience and promote each child's social, emotional, physical, cognitive and spiritual development, with special emphasis on music and worship.</p>
+							</div>
+							<div class="col-xs-12 col-sm-6">
+								<p>We view parents &amp; guardians as our partners in the education of their children, and will work beside them to reach the highest potential of early education for their child.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+		<div class="container" id="classes">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<div class="well">
+						<h2>3-year-old Classes</h2>
+						<img src="-/img/768/photo-8.jpg" alt="TFH Kids Preschool">
+						<!--<div class="price">
+							<p class="lead">$200/mo.</p>
+							<p>Five sessions per week: $475/mo.</p>
+						</div>
+						<hr>-->
+						<div class="time">
+							<h3>Monday &amp; Wednesday</h3>
+							<p>8:45 – 11:45am</p>
+							<h3>or</h3>
+							<h3>Tuesday &amp; Thursday</h3>
+							<p>8:45 – 11:45am</p>
+						</div>
+						<hr>
+						<p class="lead">Our 3-year-old program provides the children with age appropriate activities and learning skills. Children are taught basic academics such as colors, shapes, number and letter recognition as well as problem solving skills. Children are encouraged to pray for each other as well as their families. Bible stories are taught throughout&nbsp;the&nbsp;week.</p>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-6">
+					<div class="well">
+						<h2>4-year-old Classes</h2>
+						<img src="-/img/768/photo-1.jpg" alt="TFH Kids Preschool">
+						<!--<div class="price">
+							<p class="lead">$290/mo.</p>
+							<p>Five sessions per week: $475/mo.</p>
+						</div>
+						<hr>-->
+						<div class="time">
+							<h3>Monday, Wednesday, &amp; Friday</h3>
+							<p>8:45 – 11:45am</p>
+							<h3>or</h3>
+							<h3>Tuesday &amp; Thursday</h3>
+							<p>8:45 – 11:45am</p>
+						</div>
+						<hr>
+						<p class="lead">Our 4-year-old program provides the children with opportunities to explore art, math and science materials. The children are taught to write, recognize and learn the phonetic sound for each letter of the alphabet. Children are encouraged to pray for each other as well as their families. Bible stories are taught throughout&nbsp;the&nbsp;week.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<div class="well">
+						<h2>Pre-Kindergarten Class</h2>
+						<img src="-/img/768/photo-5.jpg" alt="TFH Kids Preschool">
+						<!--<div class="price">
+							<p class="lead">$475/mo.</p>
+						</div>
+						<hr>-->
+						<div class="time">
+							<h3>Monday – Friday</h3>
+							<p>8:45 – 11:45am</p>
+						</div>
+						<hr>
+						<p class="lead">Our pre-Kindergarten class is designed to prepare your child academically, emotionally, socially and spiritually for kindergarten. Teachers will use Zoo-phonics and Math Their Way to teach language and math skills. Children will be encouraged to lead and pray for each other as well as their families. Bible stories are taught throughout&nbsp;the&nbsp;week.</p>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-6">
+					<div class="well">
+						<h2>Afternoon Class</h2>
+						<img src="-/img/768/photo-9.jpg" alt="TFH Kids Preschool">
+						<!--<div class="price">
+							<p class="lead">$10/day</p>
+						</div>
+						<hr>-->
+                        <div class="time">
+                            <h3>3 &amp; 4 Year Olds
+						      <h3>Tuesday, Wednesday, &amp; Thursday</h3>
+							<p>12:30 – 3:30pm</p>
+						<!--<hr>
+						<p class="lead">One extra hour of care and lunch is served.</p>-->
+                        </div>
+						
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6"><img src="-/img/768/photo-7.jpg" alt="TFH Kids Preschool"></div>
+				<div class="col-xs-12 col-sm-6"><img src="-/img/768/photo-6.jpg" alt="TFH Kids Preschool"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12" id="contact">
+					<p class="lead">For more info, contact:</p>
+					<h1>Pamela Conner</h1>
+					<h2>(707) 455-7790 <span class="text-muted">x1594</span></h2>
+					<h2><a href="mailto:pam.conner@tfh.org">pam.conner@tfh.org</a></h2>
+				</div>
+			</div>
+
+			<div class="footer">
+				<div class="row">
+					<div class="col-xs-12">
+						<hr>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<p>&copy; TFH Kids Preschool <?php echo date('Y', strtotime('this year')); ?></p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<p><a href="https://goo.gl/maps/4njEv48Wug92" title="Get Directions">4800 Horsecreek Dr. Vacaville, CA 95688</a></p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<p>LIC #483009177</p>
+					</div>
+				</div>
+			</div>
+		</div> <!-- /container -->
+
+		<script src="http://code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
